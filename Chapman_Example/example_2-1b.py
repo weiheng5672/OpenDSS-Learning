db670c01f3d2fd051b8dd44da2f5a853b80d4232
weiheng5672 <143916114+weiheng5672@users.noreply.github.com>
2026-03-08 23:30:30 +0800

    Update example_2-1b.py

--- Chapman_Example/example_2-1b.py
+++ Chapman_Example/example_2-1b.py
@@ -1,6 +1,6 @@
 
 import math
-
+import numpy as np
 from dss import DSS as dss_engine
 
 V_low_single = 0.48 # kV
@@ -33,7 +33,7 @@ text.Command = f"Edit Vsource.source bus1=SourceBus basekv={V_low_line} pu=1.0 A
 
 text.Command = "New Transformer.T1 phases=3 windings=2"
 text.Command = "~ buses=[SourceBus, HighBus1]"
-text.Command = f"~ conns=[wye, wye] kvs=[{V_low_line:.6f}, {V_high_line:.6f}] kvas=[{S_total}, {S_total}]"
+text.Command = f"~ conns=[wye, wye] kvs=[{V_low_line:.6f}, {V_high_line:.6f}] kvas=[{2*S_total}, {2*S_total}]"
 text.Command = f"~ XHL={x_percent:.6f} %Rs=[{r_percent:.6f}, 0]"
 
 text.Command = f"New Line.L12 Bus1=HighBus1 Bus2=HighBus2 Phases=3 r1={r_line} x1={x_line} length=1 units=none"
@@ -64,7 +64,7 @@ for name in circuit.AllBusNames:
     
     print(f"{name:<10} | {pu_v:.6f} pu")
 
-print("="*30)
+print("-"*30)
 
 # 將 L12 設為目前運作的元件
 circuit.SetActiveElement("Line.L12")
@@ -79,34 +79,80 @@ i_a_ang = currents[1]  # A相電流角度 (Degree)
 print(f"L12 A相電流: {i_a_mag:.2f} A, 角度: {i_a_ang:.2f}°")
 print(f"線路L12每相的實功損失: {line_losses[0]/3:.4f} W")
 
-#
 
-tr = circuit.Transformers
-print(tr)
+# =========================================================
+print("=" * 100)
+# =========================================================
 
-trs = circuit.Transformers.AllNames
-print(trs)
 
-tr_capacity = circuit.Transformers.kva
-print(tr_capacity)
+# 打印出電路每個匯流排的名稱
+print(circuit.AllBusNames)
 
-name = circuit.Transformers.Next
-print(name)
+# 打印出電路每個匯流排的電壓大小(標么值)
+print(circuit.AllBusVmagPu)
 
-tr_name = circuit.Transformers.First
-print(tr_name)
+# -------------------------------------------------------
+# 將 OpenDSS 介面原始數據轉為 NumPy 陣列，啟用「向量化」運算
+# 轉換後方可進行矩陣計算 (如電壓比較)，而非僅限於唯讀查看
+# -------------------------------------------------------
+print(np.array(circuit.AllBusVmagPu))
+
+# 打印出電路每個匯流排的電壓大小
+print(circuit.AllBusVmag)
+
+# 打印出電路每個匯流排的複電壓(直角坐標)
+print(circuit.AllBusVolts)
+
+
+# =========================================================
+print("=" * 100)
+# =========================================================
+
+# 打印出變壓器這個物件類型
+print(circuit.Transformers)
+
+# 打印出所有變壓器名稱
+print(circuit.Transformers.AllNames)
+
+# 打印出變壓器容量
+# 如果變壓器兩繞組容量不同 會以小的繞組的容量為準
+print(circuit.Transformers.kva)
+# 但本電路有兩個電壓器 這是哪一個的容量?
+
+# --------------------------------------------
+# 有別於前一個區塊 針對匯流排 可以直接打印出全電路 匯流排電壓
+# 或許是因為變壓器本身較複雜 除了名稱以外 並不支援 一次打印出所有變壓器的相關參數
+# 技術文件中表示 First 這個API 是將第一個變壓器設為主動(active)
+# 就我的理解他的意思應該是 把注意力放在第一個變壓器的意思
+print(circuit.Transformers.First)
+print(circuit.Transformers.kVA)
+
+# 把注意力放在下一個(第二個)變壓器
+print(circuit.Transformers.Next)
+print(circuit.Transformers.kVA)
+
+# 把注意力放在下一個(第三個)變壓器 但已經沒有了 所以會打印出零
+print(circuit.Transformers.Next)
+
+# 但是變壓器容量還是會有 而且是第二個變壓器的容量
+# 所以看來如果不指定變壓器 預設打印出最後一個的容量
+print(circuit.Transformers.kVA)
+
+# 這個部分 加總變壓器的容量 從第一個變壓器開始逐步加總
+
+tr_order = circuit.Transformers.First
 
 total_tr_capacity_kva = 0
-while tr_name:
+while tr_order:
     total_tr_capacity_kva += circuit.Transformers.kva
-    tr_name = circuit.Transformers.Next
+    tr_order = circuit.Transformers.Next
     
 print(total_tr_capacity_kva)
 
+# =========================================================
+print("=" * 100)
+# =========================================================
 
+print(circuit.Lines)
 
-line = circuit.Lines
-print(line)
-
-lines = circuit.Lines.AllNames
-print(lines)
+print(circuit.Lines.AllNames)
