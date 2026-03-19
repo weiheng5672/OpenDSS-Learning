2c429a64df7320ab0e3ec3c8e564421f1469212b
weiheng <cat5672@gmail.com>
2026-03-19 15:51:41 +0800

    Create chp6_Q_11.py

---
+++ Saadat_Example/chp6_Q_11.py
@@ -0,0 +1,68 @@
+import dss
+
+V_base = 161 # kV
+S_base = 100 # MVA
+Z_base = (V_base**2) / S_base
+
+# OpenDSS 需要實際阻抗 (Ohm)，需要將 pu 轉為 Ohm
+r12_pu = 0.12
+x12_pu = 0.16
+r12 = r12_pu*Z_base
+x12 = x12_pu*Z_base
+
+P_Mw = 100 
+Q_Mvar = 50
+
+
+# 初始化 OpenDSS
+dss_engine = dss.DSS
+text = dss_engine.Text
+circuit = dss_engine.ActiveCircuit
+
+# 1. 建立電路 (設定 Bus 1 為 SwingBus)
+# BasekV 是線電壓
+
+text.Command = "Clear"
+
+text.Command = f"New Circuit.Question_6_11 basekv={V_base} phases=3 "
+
+text.Command = f"Edit Vsource.source bus1=Swing_Bus basekv={V_base} pu=1.0 Angle=0 BaseFreq=60 MVAsc3=1e12 MVASC1=1e12"
+
+# 2. 定義線路 (使用阻抗矩陣，長度設為 1)
+
+text.Command = f"New Line.L12 Bus1=Swing_Bus Bus2=Load_Bus Phases=3 r1={r12} x1={x12} length=1 units=none"
+
+
+# 3. 定義負荷 (Load)
+# OpenDSS 預設是平衡三相，kV 設定為線電壓
+text.Command = f"New Load.Load2 Bus1=Load_Bus Phases=3 kV={V_base} kW={P_Mw*1000} kvar={Q_Mvar*1000} model=1"
+
+text.Command = f"Set VoltageBases = [{V_base}]"
+text.Command = "CalcVoltageBases"
+
+circuit.Solution.Solve()
+
+# 5. 輸出結果
+bus_names = circuit.AllBusNames
+
+print(bus_names)
+
+circuit.SetActiveBus("Load_Bus")
+
+v_pu = circuit.ActiveBus.puVoltages
+
+print(f"{[v_pu[0]]}, {[v_pu[1]]}")
+
+import cmath
+# A 相 (index 0, 1)
+v_a = complex(v_pu[0], v_pu[1])
+v_b = complex(v_pu[2], v_pu[3])
+v_c = complex(v_pu[4], v_pu[5])
+mag_a, ang_rad_a = cmath.polar(v_a)
+mag_b, ang_rad_b = cmath.polar(v_b)
+mag_c, ang_rad_c = cmath.polar(v_b)
+
+import math
+print(f"Voltage: {mag_a:.4f} pu, Angle: {math.degrees(ang_rad_a):.2f} deg")
+print(f"Voltage: {mag_b:.4f} pu, Angle: {math.degrees(ang_rad_b):.2f} deg")
+print(f"Voltage: {mag_c:.4f} pu, Angle: {math.degrees(ang_rad_c):.2f} deg")
